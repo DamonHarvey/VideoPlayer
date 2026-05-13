ca55d615f37d8548e4f75cd8fc714680b605cdd8
Damon Harvey <damonjamesharvey@gmail.com>
2026-05-13 02:23:48 -0500

    More testing.

--- test.py
+++ test.py
@@ -1,26 +1,50 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout
 
 
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
-    def __init__(self):
+
+    def __init__(self) -> None:
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+        self.init_ui()
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+    def init_ui(self):
+        self.setWindowTitle("video player")
 
-        self.setFixedSize(QSize(200, 100))
+        self.init_buttons()
+
+    def init_buttons(self):
+
+        button = QPushButton()
+        button.setText("click")
+        button.clicked.connect(lambda: print("clicked"))
+        button.setFixedSize(QSize(100, 30))
+
+        button_two = QPushButton()
+        button_two.setText("who")
+        button_two.clicked.connect(lambda: print("clicked"))
+        button_two.setFixedSize(QSize(100, 30))
+
+        layout = QGridLayout()
+        layout.addWidget(button, 0, 0)
+        layout.addWidget(button_two, 0, 1)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
 
 
-app = QApplication(sys.argv)
+def main():
 
-window = MainWindow()
-window.show()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
-app.exec()
+
+if __name__ == "__main__":
+    main()
